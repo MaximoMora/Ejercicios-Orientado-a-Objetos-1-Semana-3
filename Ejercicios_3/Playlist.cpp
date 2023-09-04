10f1d57b1a6365f3a63aaf8235ee4d9353b69396
StillEffect6 <maxmora401@gmail.com>
2023-09-04 20:53:48 -0300

    Hay mejorar de que si se borra y la cancion esta en reproduccion, pausa o detenido, hay dos soluciones que cambie de cancion o encontrar el metodode cambiar el estado de la cancion, Agregar un pocas cosas mas, pero esta casi listo

--- Ejercicios_3/Playlist.cpp
+++ Ejercicios_3/Playlist.cpp
@@ -6,7 +6,7 @@
 
 
 class Playlist {
-    public:
+public:
     std::string _name;
     std::vector<std::string> _listOfSongs;
     std::string _state;
@@ -16,7 +16,7 @@ class Playlist {
     Playlist(std::string name, std::vector<std::string> listOfSongs, std::string state, int indexSong) {
         _name = name;
         int lenght = listOfSongs.size();
-        for (int i = 0; i < lenght; i = i +1) {
+        for (int i = 0; i < lenght; i = i + 1) {
             _listOfSongs.push_back(listOfSongs[i]);
         }
         _state = state;
@@ -27,7 +27,7 @@ class Playlist {
         std::cout << " " << std::endl;
         std::cout << "Playlist:" << _name << std::endl;
         std::cout << "Estado:" << _state << std::endl;
-        std::cout << "Cancion:" << _listOfSongs[_indexSong] << std::endl; 
+        std::cout << "Cancion:" << _listOfSongs[_indexSong] << std::endl;
     }
     void AddSong(std::string newSong) {
         _listOfSongs.push_back(newSong);
@@ -35,9 +35,9 @@ class Playlist {
 
     void DeleteSong(std::string deleteSong) {
         int lenght = _listOfSongs.size();
-        for (int i = 0; i < lenght; i = i +1 ) {
-            if (_listOfSongs[i] == deleteSong ) {
-                std::cout << "La Cancion: " << deleteSong <<  " se elimino " <<std::endl;
+        for (int i = 0; i < lenght; i = i + 1) {
+            if (_listOfSongs[i] == deleteSong) {
+                std::cout << "La Cancion: " << deleteSong << " se elimino " << std::endl;
 
                 _listOfSongs.erase(_listOfSongs.begin() + i);
 
@@ -46,27 +46,29 @@ class Playlist {
     }
 
     void ShowPlaylist() {
-        std::cout << "" << std::endl <<_name << std::endl;
+        std::cout << "" << std::endl << _name << std::endl;
         int lenght = _listOfSongs.size();
-        for(int i = 0; i < lenght; i = i+1 ) {
-            std::cout << i << ":"<< _listOfSongs[i] << std::endl;
+        for (int i = 0; i < lenght; i = i + 1) {
+            std::cout << i << ":" << _listOfSongs[i] << std::endl;
         }
         std::cout << " " << std::endl;
     }
 
     void PlaySongByName(std::string song) {
         int lenght = _listOfSongs.size();
-        for (int i = 0; i < lenght; i = i +1) {
+        for (int i = 0; i < lenght; i = i + 1) {
             if (_listOfSongs[i] == song) {
                 _indexSong = i;
             }
         }
         _state = "reproduciendo";
 
-        std::cout << _state <<  " " <<song << std::endl;
+        std::cout << _state << " " << song << std::endl;
     }
 
-
+    void PlaySong() {
+        _state = "reproduciendo";
+    }
 
     void PauseSong() {
         _state = "pausa";
@@ -90,7 +92,7 @@ class Playlist {
         _state = "reproduciendo";
         int lenght = _listOfSongs.size();
         std::random_device rd;
-        std::mt19937 gen(rd()); 
+        std::mt19937 gen(rd());
         std::uniform_int_distribution<int> distribution(1, lenght);
         int randomNumber = distribution(gen);
         _indexSong = randomNumber;
@@ -117,7 +119,7 @@ void MenuMain() {
 
 
 void MenuPlaylist() {
-    std::cout << "1  - Añadir Cancion" << std::endl;
+    std::cout << "1  - Agregar Cancion" << std::endl;
     std::cout << "2  - Eliminar Cancion" << std::endl;
     std::cout << "3  - Ver Canciones" << std::endl;
     std::cout << "4  - Reproducir Cancion" << std::endl;
@@ -129,45 +131,97 @@ void MenuPlaylist() {
     std::cout << "10 - Cancion Aleatoria" << std::endl;
     std::cout << "11 - Estado Playlist" << std::endl;
     std::cout << "12 - Estado Cancion" << std::endl;
-    std::cout << "13 - Cambiar Nombre de la playlist" std::endl;
+    std::cout << "13 - Cambiar Nombre de la playlist" << std::endl;
     std::cout << "0 - Salir" << std::endl;
 
 }
 
 void CreatePlaylist() {
     std::cout << "Nombre de la playlist" << std::endl << "> ";
+
 }
+
+
+Playlist playlist_1("Lista de canciones", {}, "Detenido", 0);
+
 
 int main() {
 
-    int userOption = -1;
+    std::string newSong;
+    std::string deleteSong;
+    std::string playSong;
 
-    Playlist playlist_1("Lista de canciones",{},"Detenido",0);
+    int userOption = -1;
 
     std::string namePlaylist;
     CreatePlaylist();
     std::cin >> namePlaylist;
     playlist_1.Set_name(namePlaylist);
 
-    while ( userOption != 0) {
+    while (userOption != 0) {
 
         MenuPlaylist();
         std::cout << "> ";
         std::cin >> userOption;
+        std::cout << typeid(userOption).name() << std::endl;
+        switch (userOption)
+        {
+        case 1 == 1:
+            std::cout << "Cancion a agregar: ";
+            std::cin >> newSong;
+            playlist_1.AddSong(newSong);
+            continue;
 
-        if 
+        case 2:
+            std::cout << "Cancion a eliminar: ";
+            std::cin >> deleteSong;
+            playlist_1.DeleteSong(deleteSong);
+            continue;
 
+        case 3:
+            playlist_1.ShowPlaylist();
+            continue;
 
+        case 4:
+            playlist_1.PlaySong();
+            continue;
 
-        //if (userOption == 1) {
-        //    std::cout << "Nombre de la playlist";
-        //    std::cin >> nameOfClass;
-        //    Playlist nameOfClass;
-        //}elif (userOption == 1) {
+        case 5:
+            std::cout << "Cancion a reproducir: ";
+            std::cin >> playSong;
+            continue;
 
-        //}
+        case 6:
+            playlist_1.PauseSong();
+            continue;
 
+        case 7:
+            playlist_1.StopSong();
+            continue;
+
+        case 8:
+            playlist_1.NextSong();
+            continue;
+
+        case 9:
+            playlist_1.BeforeSong();
+            continue;
+
+        case 10:
+            playlist_1.RandomSong();
+            continue;
+
+        case 11:
+            playlist_1.State();
+            continue;
+
+        default:
+            std::cout << "Eliga otra opcion" << std::endl;
+            break;
+        }
 
     }
+
+    return 1;
 
 }
